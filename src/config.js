603ee24383f0a3fc0c526e6390bcdcc87fed4f54
harper.shapiro <harper.shapiro@gmail.com>
2020-10-17 18:29:51 -0500

    added public config file to branch

---
+++ src/config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+//const DEV_API_PATH = "http://localhost:4000/molten"
+//const PROD_API_PATH = ""
+config.API_PATH = (process.env.NODE_ENV==="development" ? "http://localhost:4000/molten" : process.env.API_PATH)
+
+module.exports = config;
