638c02302767c3a2e486c6bd8b8dd0e9a144ad62
harper.shapiro <harper.shapiro@gmail.com>
2020-10-17 21:54:45 -0500

    basic express server to serve from build folder

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ var config = {};
 
 //const DEV_API_PATH = "http://localhost:4000/molten"
 //const PROD_API_PATH = ""
-console.log(process.env.API_PATH)
 config.API_PATH = (process.env.NODE_ENV==="development" ? "http://localhost:4000/molten" : process.env.API_PATH)
 
 module.exports = config;
