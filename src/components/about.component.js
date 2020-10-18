03e8e953e554a9813c06842279d4fad7fc81c2bd
harper.shapiro <harper.shapiro@gmail.com>
2020-10-18 15:48:50 -0500

    small test in about

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 export default class About extends Component {
     render(){
+        console.log("API PATH: " + process.env.API_PATH)
         return (
             <div>
                 <h1> About </h1>
